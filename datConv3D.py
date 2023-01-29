9ade9c9d2b69e23bb913375d3d98f053afdc2f45
DehnP <114683299+DehnP@users.noreply.github.com>
2023-01-29 23:05:56 +0000

    3DFirstBuild

--- datConv3D.py
+++ datConv3D.py
@@ -2,12 +2,37 @@ import tkinter
 from tkinter import filedialog
 from matplotlib.figure import Figure
 import customtkinter as ctk
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg)
 import numpy as np
 from datFuncs3D import *
-from datConvClasses import *
 from PIL import Image
 import os
+from dataclasses import dataclass
+import pickle
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+
+
+class Blade:
+    def __init__(self,sections=[]):
+        self.sections = sections # list of sections
+    
+    def add_section(self, section):
+        # check if a section with the same n value exists
+        for i, s in enumerate(self.sections):
+            if s.n == section.n:
+                self.sections.pop(i) # remove existing section
+                break
+        self.sections.append(section) # add new section to list of sections
+
+@dataclass
+class Section:
+    n: int# section number
+    dr: float # displacement
+    twist: float# twist angle
+    chord: float# chord length
+    profile: np.array# profile coordinates
+    coords: np.array# 3D coordinates
 
 class App(ctk.CTk):
     def __init__(self):
@@ -16,12 +41,12 @@ class App(ctk.CTk):
         ctk.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
         ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
         self.resizable(0,0)
-        self.geometry("1000x600")
+        # self.geometry("1000x600")
         self.title("datConv")
         self.iconbitmap("Assets/datCon.ico")
         # initialize the window
         self.mainView()
-
+        self.currentBlade = Blade()
 
     def mainView(self):
         titlesFont = ctk.CTkFont(family="Arciform", size=20)
@@ -38,7 +63,7 @@ class App(ctk.CTk):
         self.tabView = ctk.CTkTabview(self.masterFrame)
         self.tabView.pack(fill='both',expand=True,padx=5,pady=5,side='left')
         self.configTab = self.tabView.add("Config")
-        self.saveTab = self.tabView.add("Save/Load")
+        self.importTab = self.tabView.add("Import")
 
         #=====================Config==================================================
         self.configFrame = ctk.CTkFrame(self.configTab)
@@ -79,18 +104,18 @@ class App(ctk.CTk):
         self.sectionSelectOption.set(1)
 
         # profile Twist
-        self.profileTwistLabel = ctk.CTkLabel(self.profileConfigFrame,text="Twist: ",font=textFont)
-        self.profileTwistLabel.pack(side='left',padx=5,pady=5)
-        self.profileTwistEntry = ctk.CTkEntry(self.profileConfigFrame,font=textFont,width=55)
-        self.profileTwistEntry.pack(side='left',padx=5,pady=5)
-        self.profileTwistEntry.insert(0,"0")
+        self.sectionTwistLabel = ctk.CTkLabel(self.profileConfigFrame,text="Twist: ",font=textFont)
+        self.sectionTwistLabel.pack(side='left',padx=5,pady=5)
+        self.sectionTwistEntry = ctk.CTkEntry(self.profileConfigFrame,font=textFont,width=55)
+        self.sectionTwistEntry.pack(side='left',padx=5,pady=5)
+        self.sectionTwistEntry.insert(0,"0")
 
         # profile Chord
-        self.profileChordLabel = ctk.CTkLabel(self.profileConfigFrame,text="Chord: ",font=textFont)
-        self.profileChordLabel.pack(side='left',padx=5,pady=5)
-        self.profileChordEntry = ctk.CTkEntry(self.profileConfigFrame,font=textFont,width=55)
-        self.profileChordEntry.pack(side='left',padx=5,pady=5)
-        self.profileChordEntry.insert(0,"1")
+        self.sectionChordLabel = ctk.CTkLabel(self.profileConfigFrame,text="Chord: ",font=textFont)
+        self.sectionChordLabel.pack(side='left',padx=5,pady=5)
+        self.sectionChordEntry = ctk.CTkEntry(self.profileConfigFrame,font=textFont,width=55)
+        self.sectionChordEntry.pack(side='left',padx=5,pady=5)
+        self.sectionChordEntry.insert(0,"1")
 
         # profile select
         self.profileSelectLabel = ctk.CTkLabel(self.profileConfigFrame,text="Profile: ",font=textFont)
@@ -108,12 +133,36 @@ class App(ctk.CTk):
         self.profileSaveButton = ctk.CTkButton(self.profileConfigFrame,text="Save",font=textFont,command=self.save_section_config_event)
         self.profileSaveButton.pack(side='right',padx=5,pady=5)
 
+        # save Blade Frame
+        self.saveBladeFrame = ctk.CTkFrame(self.configFrame)
+        self.saveBladeFrame.pack(fill='x',expand=True,padx=5,pady=5,anchor='n')
+        
+        # save Blade Button
+        self.saveBladeButton = ctk.CTkButton(self.saveBladeFrame,text="Save Blade",font=textFont,command=self.save_blade_event)
+        self.saveBladeButton.pack(side='right',padx=5,pady=5)
 
+        # save Blade Name Entry
+        self.saveBladeNameEntry = ctk.CTkEntry(self.saveBladeFrame,font=textFont,width=100)
+        self.saveBladeNameEntry.pack(side='right',padx=5,pady=5)
+        self.saveBladeNameEntry.insert(0,"Blade Name")
 
+        # plot Blade Frame
+        self.plotBladeFrame = ctk.CTkFrame(self.configFrame)
+        self.plotBladeFrame.pack(fill='x',expand=True,padx=5,pady=5,anchor='n')
+        # plot Blade Button
+        self.plotBladeButton = ctk.CTkButton(self.plotBladeFrame,text="Plot Blade",font=textFont,command=self.plot_blade_event)
+        self.plotBladeButton.pack(side='right',padx=5,pady=5)
+
+        # load Blade Frame
+        self.loadBladeFrame = ctk.CTkFrame(self.configFrame)
+        self.loadBladeFrame.pack(fill='x',expand=True,padx=5,pady=5,anchor='n')
+        # load Blade Button
+        self.loadBladeButton = ctk.CTkButton(self.loadBladeFrame,text="Load Blade",font=textFont,command=self.load_blade_event)
+        self.loadBladeButton.pack(side='right',padx=5,pady=5)
 
 
         #=====================Save/Load==================================================
-        self.saveLoadFrame = ctk.CTkFrame(self.saveTab)
+        self.saveLoadFrame = ctk.CTkFrame(self.importTab)
         self.saveLoadFrame.pack(fill='both',expand=True,padx=5,pady=5)
 
         ## Save/Load Widgets
@@ -121,7 +170,7 @@ class App(ctk.CTk):
         self.saveLoadFrame.pack(fill='both',expand=True,padx=5,pady=5)
 
         self.folder_icon = ctk.CTkImage(light_image=Image.open("Assets/Folder.png"))
-        self.File_Loc_Label = ctk.CTkLabel(self.saveLoadFrame,text='File Location')
+        self.File_Loc_Label = ctk.CTkLabel(self.saveLoadFrame,text='Import .dat File Location:')
         self.File_Loc_Label.grid(row=0,column=0,columnspan=3,sticky='nsew',pady=2)
 
         self.File_Loc_Entry = ctk.CTkEntry(self.saveLoadFrame,placeholder_text='C:\\..',width=300)
@@ -142,6 +191,26 @@ class App(ctk.CTk):
         self.File_Save = ctk.CTkButton(self.saveLoadFrame,text='Save',width=50,command=self.save_profile_event)
         self.File_Save.grid(row=3,column=1,columnspan=3,sticky='nsew') 
             
+        self.Save_Loc_Label = ctk.CTkLabel(self.saveLoadFrame,text='Export SC compatible Save Location:')
+        self.Save_Loc_Label.grid(row=4,column=0,columnspan=3,sticky='nsew',pady=2)
+
+        self.Save_Loc_Entry = ctk.CTkEntry(self.saveLoadFrame,placeholder_text='D:\\..',width=300)
+        self.Save_Loc_Entry.grid(row=5,column=0,sticky='nsew',pady=2,columnspan=2)
+
+        self.Save_Loc_Open = ctk.CTkButton(self.saveLoadFrame,text='',width=20,image=self.folder_icon,command=self.open_save_location_event)
+        self.Save_Loc_Open.grid(row=5,column=2,sticky='nsew',pady=2) 
+
+        self.File_Name_Label = ctk.CTkLabel(self.saveLoadFrame,text='File Name')
+        self.File_Name_Label.grid(row=6,column=0,sticky='nsew',pady=2)
+
+
+        self.File_Name_Entry = ctk.CTkEntry(self.saveLoadFrame,placeholder_text='NACAXXXX..')
+        self.File_Name_Entry.grid(row=7,column=0,sticky='nsew',pady=2)
+
+
+        self.File_Save = ctk.CTkButton(self.saveLoadFrame,text='Save',width=50,command=self.export_curves_event)
+        self.File_Save.grid(row=7,column=1,columnspan=3,sticky='nsew') 
+
 
         ##=====================rightFrame==================================================
         self.plotViewLabel = ctk.CTkLabel(self.rightFrame,text="Plot View",font=titlesFont)
@@ -150,6 +219,37 @@ class App(ctk.CTk):
         self.plotViewFrame = ctk.CTkFrame(self.rightFrame)
         self.plotViewFrame.pack(fill='both',expand=True,padx=10,pady=10)
 
+##=====================METHODS==================================================
+    def open_save_location_event(self):
+        root = tkinter.Tk()
+        root.withdraw() #use to hide tkinter window
+        tempdir = filedialog.askdirectory(parent=root, title='Please select a folder')
+        if len(tempdir) > 0:
+            self.Save_Loc_Entry.delete(0,'end')
+            self.Save_Loc_Entry.insert(0, tempdir)
+
+    def export_curves_event(self):
+        # Export curves to SC compatible format
+        print("export_curves_event")
+        
+        # create temp array
+        tempArray = np.array(self.currentBlade.sections[0].coords)
+        tempFullArray = tempArray
+
+        # read rest of sections and add to tempFullArray
+        for i in range(1,len(self.currentBlade.sections)):
+            tempArray = np.array(self.currentBlade.sections[i].coords)
+            # append to tempFullArray
+            tempFullArray = np.concatenate((tempFullArray, tempArray), axis=0)
+        print(tempFullArray)
+        # round values in tempFullArray to 8 decimal places
+        tempFullArray = np.around(tempFullArray, decimals=8)
+
+        # save to file
+        np.savetxt(self.Save_Loc_Entry.get() + "\\" + self.File_Name_Entry.get() + ".txt", tempFullArray, delimiter=",",fmt='%.6f')
+        add_blank_line(self.Save_Loc_Entry.get() + "\\" + self.File_Name_Entry.get() + ".txt")
+        prepend_line_to_file(self.Save_Loc_Entry.get() + "\\" + self.File_Name_Entry.get() + ".txt", "3d=true")
+    
     def change_number_of_profile_event(self):
         print("change_number_of_profile_event")
         # get value from profile quantity entry
@@ -161,6 +261,48 @@ class App(ctk.CTk):
     def save_section_config_event(self):
         # Save configuration of section
         print("save_profile_event")
+        # get values from entry boxes
+        sectionIndex = int(self.sectionSelectOption.get())
+        sectionDisplacement = float(self.profileDisplacementEntry.get())
+        sectionTwist = float(self.sectionTwistEntry.get())
+        sectionChord = float(self.sectionChordEntry.get())
+        # strip the .txt from the profile name
+        tempProfileName = self.profileSelectOption.get()
+        sectionProfile = strip_profile_to_array("Profiles/" + tempProfileName + ".txt")
+
+        # generate coords for profile
+        sectionCoords = scale_profile(sectionProfile,sectionChord)
+        sectionCoords = rotate_profile(sectionCoords,sectionTwist)
+        sectionCoords = add_displacement_column(sectionCoords,sectionIndex,sectionDisplacement)
+
+        # save to class 'section' object
+        tempSec = Section(sectionIndex,sectionDisplacement,sectionTwist,sectionChord,sectionProfile,sectionCoords)
+
+        # save to class 'blade' object
+        self.currentBlade.add_section(tempSec)
+
+        print(tempSec)
+        print(self.currentBlade.sections)
+
+
+    def save_blade_event(self):
+        # save blade to file
+        print("save_blade_event")
+        # get blade name from entry box
+        bladeName = self.saveBladeNameEntry.get()
+        with open('Blades/' + bladeName + '.txt', 'wb') as f:
+            pickle.dump(self.currentBlade, f)
+
+    def load_blade_event(self):
+        # load blade from file
+        print("load_blade_event")
+        root = tkinter.Tk()
+        root.withdraw()
+        tempdir = filedialog.askopenfilename(parent=root, title='Please select a file',filetypes=[('Text file','.txt')])
+        if len(tempdir) > 0:
+            with open(tempdir, 'rb') as f:
+                self.currentBlade = pickle.load(f)
+    
 
     def load_profile_event(self):
         # load dat file
@@ -184,7 +326,7 @@ class App(ctk.CTk):
             print("No File Name")
             return
         else:
-            array = strip_txt_to_array(self.File_Loc_Entry.get())
+            array = strip_dat_to_array(self.File_Loc_Entry.get())
             savePath = ("Profiles/"+self.File_Name_Entry.get()+".txt")
             np.savetxt(savePath, array,fmt='%1.6f',delimiter=",")
 
@@ -194,6 +336,45 @@ class App(ctk.CTk):
         for file in os.listdir("Profiles"):
             if file.endswith(".txt"):
                 self.profileList.append(file.replace(".txt",""))
+
+    def plot_blade_event(self):
+        # plot blade
+        print("plot_blade_event")
+        # concatenate all sections
+        bladeCoords = np.array([])
+        for section in self.currentBlade.sections:
+            if bladeCoords.size == 0:
+                bladeCoords = section.coords
+            else:
+                bladeCoords = np.concatenate((bladeCoords,section.coords),axis=0)
+                
+        ## plot 3d blade on plotView canvas 
+        # clear canvas
+        self.plotViewFrame.destroy()
+        self.plotViewFrame = ctk.CTkFrame(self.rightFrame)
+        self.plotViewFrame.pack(fill='both',expand=True,padx=10,pady=10)
+        # plot
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(bladeCoords[:,0],bladeCoords[:,1],bladeCoords[:,2])
+        # keep axes equal
+        max_range = np.array([bladeCoords[:,0].max()-bladeCoords[:,0].min(), bladeCoords[:,1].max()-bladeCoords[:,1].min(), bladeCoords[:,2].max()-bladeCoords[:,2].min()]).max() / 2.0
+        mid_x = (bladeCoords[:,0].max()+bladeCoords[:,0].min()) * 0.5
+        mid_y = (bladeCoords[:,1].max()+bladeCoords[:,1].min()) * 0.5
+        mid_z = (bladeCoords[:,2].max()+bladeCoords[:,2].min()) * 0.5
+        ax.set_xlim(mid_x - max_range, mid_x + max_range)
+        ax.set_ylim(mid_y - max_range, mid_y + max_range)
+        ax.set_zlim(mid_z - max_range, mid_z + max_range)
+        # plot on canvas
+        canvas = FigureCanvasTkAgg(fig, master=self.plotViewFrame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+        toolbar = NavigationToolbar2Tk(canvas, self.plotViewFrame)
+        toolbar.update()
+        canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+
+
 
 
         
--- datFuncs3D.py
+++ datFuncs3D.py
@@ -1,15 +1,63 @@
 import numpy as np
 import os
+import pandas as pd
 
-# Function to read in the data from the .txt files
-def strip_txt_to_array(file_path):
+# Function to read in the data from dat .txt files
+def strip_dat_to_array(file_path):
     array = []
     with open(file_path) as text_file:
         lines = text_file.readlines()
-    del lines[0]
+    del lines[0] # remove header
     for line in lines:
         array1 = [item.strip() for item in line.split()]
         array.append(array1)
     array = np.asarray(array, dtype=np.float16)    
     return array
 
+# Function to strip profile coordinates to a numpy array from a .txt file
+def strip_profile_to_array(file_path):
+    array = np.loadtxt(file_path,delimiter=',', dtype=np.float16)
+    return array
+
+# Function to scale a profile coordinates by a scale factor
+def scale_profile(profile, scale_factor: float):
+    profile = profile * scale_factor
+    return profile
+
+# function to rotate a profile by a given angle about 0,0
+def rotate_profile(profile, angle: float):
+    theta = np.radians(angle)
+    c, s = np.cos(theta), np.sin(theta)
+    R = np.array(((c,-s), (s, c)))
+    profile = np.dot(R, profile.T).T
+    return profile
+
+
+# Function which adds a column (displacement x section index) to the left of the array
+def add_displacement_column(array,n,dr):
+    radialDist = dr * n
+    array = np.insert(array, 0, radialDist, axis=1)
+    return array
+
+# Function to add a blank line after every 35th line in a .txt file
+def add_blank_line(file_path):
+    lines = []
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+
+    with open(file_path, "w") as file:
+        line_count = 1
+        for line in lines:
+            file.write(line)
+            if line_count % 35 == 0:
+                file.write("\n")
+            line_count += 1
+
+def prepend_line_to_file(file_path, line):
+    dummy_file = file_path + '.bak'
+    with open(file_path, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
+        write_obj.write(line + '\n')
+        for line in read_obj:
+            write_obj.write(line)
+    os.remove(file_path)
+    os.rename(dummy_file, file_path)
